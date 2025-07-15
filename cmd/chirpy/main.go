19b2c97c3c1d945853096901faedbfd66749ca2d
Leon Lonsdale <coding@leonlonsdale.dev>
2025-07-15 17:02:43 +0100

    refactor(main): refactor to server creation - remove unnecessary custom Server struct

--- cmd/chirpy/main.go
+++ cmd/chirpy/main.go
@@ -8,10 +8,12 @@ import (
 )
 
 func main() {
-	s := server.NewServer()
+	const port = "8080"
+
+	s := server.NewServer(port)
 	fmt.Println("Starting server on 8080")
 
-	if err := s.Start(); err != nil {
+	if err := s.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 }
