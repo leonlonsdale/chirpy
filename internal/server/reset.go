729130e5ec63761a84266df2a94ed389fa306de5
Leon Lonsdale <coding@leonlonsdale.dev>
2025-07-15 17:02:10 +0100

    feat(metrics): add /reset endpoint to reset file server hit counter

---
+++ internal/server/reset.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func registerResetHandler(mux *http.ServeMux, cfg *apiConfig) {
+	mux.HandleFunc("/reset", cfg.resetHandler)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+
+	_, _ = fmt.Fprintf(w, "Hit counter reset!")
+}
