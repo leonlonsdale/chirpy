77dce1424e67ce3bb4cd831b19328473008c2d20
Leon Lonsdale <coding@leonlonsdale.dev>
2025-07-15 21:27:27 +0100

    chore: better format html string

--- internal/server/metrics.go
+++ internal/server/metrics.go
@@ -10,10 +10,16 @@ func registerMetricsHandler(mux *http.ServeMux, cfg *apiConfig) {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	resString := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
+	resString := fmt.Sprintf(`
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`,
+		cfg.fileserverHits.Load())
 
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	_, _ = fmt.Fprint(w, resString)
-
 }
