105a396d2b670d1cef6e2aa845ae63fa49f9dc3e
Leon <coding@leonlonsdale.dev>
2025-07-16 09:37:37 +0100

    refactor(server): export ApiConfig to allow instantiation outside package. update capitalisation in methods using it

--- internal/server/reset.go
+++ internal/server/reset.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-func registerResetHandler(mux *http.ServeMux, cfg *apiConfig) {
+func registerResetHandler(mux *http.ServeMux, cfg *ApiConfig) {
 	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 }
 
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *ApiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits.Store(0)
+	cfg.FileserverHits.Store(0)
 
 	_, _ = fmt.Fprintf(w, "Hit counter reset!")
 }
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,7 +2,7 @@ package server
 
 import "net/http"
 
-func registerHandlers(mux *http.ServeMux, cfg *apiConfig) {
+func registerHandlers(mux *http.ServeMux, cfg *ApiConfig) {
 	registerHealthzHandler(mux)
 	registerMetricsHandler(mux, cfg)
 	registerResetHandler(mux, cfg)
