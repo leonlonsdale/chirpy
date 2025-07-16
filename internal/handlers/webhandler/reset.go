f661272d2e586c8adef1e7f2a64ed98cdc51b74a
Leon Lonsdale <coding@leonlonsdale.dev>
2025-07-16 19:34:54 +0100

    fix(reset): restrict reset endpoint to dev

--- internal/handlers/webhandler/reset.go
+++ internal/handlers/webhandler/reset.go
@@ -2,9 +2,11 @@ package webhandler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/leonlonsdale/chirpy/internal/config"
+	"github.com/leonlonsdale/chirpy/internal/util"
 )
 
 func RegisterResetHandler(mux *http.ServeMux, cfg *config.ApiConfig) {
@@ -13,10 +15,18 @@ func RegisterResetHandler(mux *http.ServeMux, cfg *config.ApiConfig) {
 
 func ResetHandler(cfg *config.ApiConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if cfg.Platform != "dev" {
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusForbidden)
+			log.Println("User accessed reset")
+			return
+		}
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		cfg.FileserverHits.Store(0)
-
+		if err := cfg.DBQueries.ResetUsers(r.Context()); err != nil {
+			util.RespondWithError(w, http.StatusInternalServerError, "there was a problem resetting users", err)
+		}
 		_, _ = fmt.Fprintf(w, "Hit counter reset!")
 	}
 }
