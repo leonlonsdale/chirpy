fc7ba45d7ce8ca4e0d76a46b842cf64e9a0977ed
Leon <coding@leonlonsdale.dev>
2025-07-17 23:21:08 +0100

    refactor(sql): improve refresh tokan queries

--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -1,14 +1,19 @@
 -- name: CreateRefreshToken :exec
-INSERT INTO refresh_tokens (token, created_at, updated_at, user_id, expires_at, revoked_at)
-    VALUES ($1, NOW(), NOW(), $2, $3, $4);
+INSERT INTO refresh_tokens (token, created_at, updated_at, user_id, expires_at)
+    VALUES ($1, NOW(), NOW(), $2, $3)
+RETURNING
+    *;
 
 -- name: GetRefreshToken :one
-SELECT
-    *
-FROM
+UPDATE
     refresh_tokens
+SET
+    revoked_at = NOW(),
+    updated_at = NOW()
 WHERE
-    token = $1;
+    token = $1
+RETURNING
+    *;
 
 -- name: RevokeRefreshToken :exec
 UPDATE
@@ -18,4 +23,15 @@ SET
     updated_at = NOW()
 WHERE
     token = $1;
+
+-- name: GetUserFromRefreshToken :one
+SELECT
+    users.*
+FROM
+    users
+    JOIN refresh_tokens ON users.id = refresh_tokens.user_id
+WHERE
+    refresh_tokens.token = $1
+    AND revoked_at IS NULL
+    AND expires_at > NOW();
 
