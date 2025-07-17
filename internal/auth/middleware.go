dd7a24205079e83184664d91c3867c5eeb20192a
Leon Lonsdale <coding@leonlonsdale.dev>
2025-07-17 21:28:37 +0100

    feat(auth): add jwt checking middleware to wrap handlers

---
+++ internal/auth/middleware.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/leonlonsdale/chirpy/internal/config"
+	"github.com/leonlonsdale/chirpy/internal/util"
+)
+
+type contextKey string
+
+const UserIDKey contextKey = "userID"
+
+func AuthMiddleware(cfg *config.ApiConfig, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString, err := GetBearerToken(r.Header)
+		if err != nil {
+			http.Error(w, "Unauthorized: missing or invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		userID, err := ValidateJWT(tokenString, cfg.Secret)
+		if err != nil {
+			util.RespondWithError(w, http.StatusUnauthorized, "invalid token", nil)
+
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), UserIDKey, userID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func UserIDFromContext(ctx context.Context) (uuid.UUID, bool) {
+	userIDStr, ok := ctx.Value(UserIDKey).(string)
+	if !ok {
+		return uuid.Nil, false
+	}
+
+	userID, err := uuid.Parse(userIDStr)
+	if err != nil {
+		return uuid.Nil, false
+	}
+
+	return userID, true
+}
