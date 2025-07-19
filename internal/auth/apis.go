94dc82f231b8c5545aae39a902edfdcf2534f631
Leon Lonsdale <coding@leonlonsdale.dev>
2025-07-19 17:34:32 +0100

    feat(auth): get api helper for poklka payment service

---
+++ internal/auth/apis.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+func GetAPIKey(headers http.Header) (string, error) {
+	apiKey := headers.Get("Authorization")
+
+	if apiKey == "" {
+		return "", errors.New("no authorizarion headers found")
+
+	}
+
+	key := strings.Fields(apiKey)
+	if len(key) < 2 {
+		return "", errors.New("no api key found in headers")
+	}
+
+	if key[0] != "ApiKey" {
+		return "", errors.New("no api key found in headers")
+	}
+
+	return key[1], nil
+
+}
