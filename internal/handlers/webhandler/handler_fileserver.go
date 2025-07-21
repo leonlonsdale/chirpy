7d0c265717a5a6c83b40fba5309e8ed86ab50164
Leon <coding@leonlonsdale.dev>
2025-07-21 11:59:48 +0100

    refactor(hanadlers): move healthz, fileserver, metrics and reset handlers to app methods

--- internal/handlers/webhandler/handler_fileserver.go
+++
@@ -1,9 +0,0 @@
-package webhandler
-
-import (
-	"net/http"
-)
-
-func FileServerHandler(mux *http.ServeMux, middleware func(next http.Handler) http.Handler) {
-	mux.Handle("/app/", middleware(http.StripPrefix("/app", http.FileServer(http.Dir("./web/")))))
-}
--- internal/types/types.go
+++ internal/types/types.go
@@ -10,49 +10,66 @@ import (
 // user
 
 type NewUser struct {
-	Email          string
-	HashedPassword string
+	Email          string `json:"email"`
+	HashedPassword string `json:"hashed_password"`
 }
 
 type User struct {
-	ID             uuid.UUID
-	Email          string
-	HashedPassword string
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	IsChirpyRed    bool
+	ID             uuid.UUID `json:"id"`
+	Email          string    `json:"email"`
+	HashedPassword string    `json:"hashed_password"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	IsChirpyRed    bool      `json:"is_chirpy_red"`
 }
 
 type UpdateUser struct {
-	Email          string
-	HashedPassword string
-	ID             uuid.UUID
+	Email          string    `json:"email"`
+	HashedPassword string    `json:"hashed_password"`
+	ID             uuid.UUID `json:"id"`
 }
 
 type UpdatedUser struct {
-	ID          uuid.UUID
-	Email       string
-	CreatedAt   sql.NullTime
-	UpdatedAt   sql.NullTime
-	IsChirpyRed bool
+	ID          uuid.UUID    `json:"id"`
+	Email       string       `json:"email"`
+	CreatedAt   sql.NullTime `json:"created_at"`
+	UpdatedAt   sql.NullTime `json:"updated_at"`
+	IsChirpyRed bool         `json:"is_chirpy_red"`
 }
 
 // chirps
 
 type Chirp struct {
-	ID        uuid.UUID
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Body      string
-	UserID    uuid.UUID
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 type NewChirp struct {
-	Body   string
-	UserID uuid.UUID
+	Body   string    `json:"body"`
+	UserID uuid.UUID `json:"user_id"`
 }
 
 type DeleteChirp struct {
-	ID     uuid.UUID
-	UserID uuid.UUID
+	ID     uuid.UUID `json:"id"`
+	UserID uuid.UUID `json:"user_id"`
+}
+
+// refresh token
+
+type RefreshToken struct {
+	Token     string       `json:"token"`
+	CreatedAt time.Time    `json:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at"`
+	UserID    uuid.UUID    `json:"user_id"`
+	ExpiresAt time.Time    `json:"expires_at"`
+	RevokedAt sql.NullTime `json:"revoked_at"`
+}
+
+type CreateRefreshToken struct {
+	Token     string    `json:"token"`
+	UserID    uuid.UUID `json:"user_id"`
+	ExpiresAt time.Time `json:"expires_at"`
 }
